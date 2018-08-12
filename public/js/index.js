203305c4743c2be3c441cf66f854c08f69832181
Jody Castro <jcandothers@gmail.com>
2018-08-12 11:11:42 -0500

    was linking to the incorrect id

--- public/js/index.js
+++ public/js/index.js
@@ -89984,12 +89984,12 @@ var GREYS = {
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function($) {$(document).ready(function () {
-    $('table[data-form="deleteForm"]').on('click', '.form-button-delete', function (e) {
+    $('table[data-form="deleteForm"]').on('click', '.form-delete', function (e) {
         e.preventDefault();
         var $form = $(this);
         $('#confirm').modal({ backdrop: 'static', keyboard: false }).on('click', '#delete-btn', function () {
             $form.submit();
-            console.log($form.submit());
+            console.log($form);
         });
     });
 });
--- resources/assets/scripts/deleteForm.js
+++ resources/assets/scripts/deleteForm.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    $('table[data-form="deleteForm"]').on('click', '.form-button-delete', function(e){
+    $('table[data-form="deleteForm"]').on('click', '.form-delete', function(e){
         e.preventDefault();
         var $form=$(this);
         $('#confirm').modal({ backdrop: 'static', keyboard: false })
             .on('click', '#delete-btn', function(){
                 $form.submit();
-                console.log($form.submit());
+                console.log($form);
             });
     });
 });
