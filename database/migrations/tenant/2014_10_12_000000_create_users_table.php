13d6d936f9ae1641678dd993c46772752d869084
Jody Castro <jcandothers@gmail.com>
2018-08-13 13:06:20 -0500

    fixed table

--- database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->uuid('uuid');
             $table->string('name');
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->string('password')->nullable();
             $table->timestamp('password_changed_at')->nullable();
