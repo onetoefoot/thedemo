947f3777d2f09a0717da42a8dfc1646070506b9c
Jody Castro <jcandothers@gmail.com>
2018-08-20 11:24:55 -0500

    changes

---
+++ tests/Feature/RolesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Hyn\Tenancy\Environment;
+use App\Role;
+
+class RolesTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
